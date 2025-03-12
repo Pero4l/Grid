6324c15ca9a3583c0ad0066521be1227f520b708
Peter Abahim <lilpero03@gmail.com>
2025-03-12 21:16:06 +0100

    first push

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    
+    extend: {
+      fontFamily:{
+        'custom': ["Anton", "Open Sans", "sans-serif" ]
+      }
+    },
+
+  },
+  plugins: [],
+}
